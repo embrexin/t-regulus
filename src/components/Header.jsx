27a1b83f19d804dc58200d0908e9b0d097aa68cc
embrexin <amber.jiang.xiao@gmail.com>
2026-02-08 15:27:54 -0500

    Add responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../assets/Logo.svg';
 
 function Header({ isScrolled }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu
   const navItems = ["Mission", "Technology", "ALS", "Investor Relationship", "Publications", "About Us"];
 
   const handleScrollToSection = (event, item) => {
@@ -18,7 +19,12 @@ function Header({ isScrolled }) {
         top: offsetPosition,
         behavior: "smooth"
       });
+      setIsMobileMenuOpen(false); // Close menu after clicking a link
     }
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -27,7 +33,12 @@ function Header({ isScrolled }) {
         <a href="#"><img src={Logo} alt="T-Regulus Logo" /></a>
       </div>
       <nav>
-        <ul>
+        <div className="hamburger-menu" onClick={toggleMobileMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <ul className={isMobileMenuOpen ? 'mobile-open' : ''}> {/* Add mobile-open class */}
           {navItems.map(item => (
             <li key={item}>
               <a href={`#${item.toLowerCase().replace(/ /g, '-')}`} onClick={(e) => handleScrollToSection(e, item)}>
