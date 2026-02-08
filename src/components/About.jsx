9995985f1ff1104220fcfb39192db4c3d6e8c70f
embrexin <amber.jiang.xiao@gmail.com>
2026-02-08 15:09:52 -0500

    Add second About ALS section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cell2 from '../../assets/cell2.jpg';
+import cell3 from '../../assets/cell3.jpeg';
 
 function About() {
   return (
@@ -21,6 +22,31 @@ function About() {
           </div>
           <div className="about-right">
             <img src={cell2} alt="ALS cell" />
+          </div>
+        </div>
+        <div className="about-boxes">
+          <div className="about2-left">
+            <img src={cell3} alt="Another ALS cell" />
+          </div>
+          <div className="about2-right">
+            <div className="about2-box">
+              <div className="about2-box-spacer"></div>
+              <div className="about2-box-text1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
+            <div className="about2-box">
+              <div className="about2-box-text2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+              <div className="about2-box-spacer"></div>
+            </div>
+            <div className="about2-box">
+              <div className="about2-box-spacer"></div>
+              <div className="about2-box-text3">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
